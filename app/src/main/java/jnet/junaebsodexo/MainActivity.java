341d3d30ab9b9a79958e1ede50925acacce09405
Charlie <jost1@live.com>
2018-03-10 00:45:40 -0300

    Delete Spam Code commit

--- app/src/main/java/jnet/junaebsodexo/MainActivity.java
+++ app/src/main/java/jnet/junaebsodexo/MainActivity.java
@@ -3,11 +3,10 @@ package jnet.junaebsodexo;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 
 import org.json.simple.JSONObject;
@@ -15,9 +14,6 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
-import java.sql.Date;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import static jnet.junaebsodexo.SodexoClient.getContentHTML;
@@ -27,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     SharedPreferences sharedPref = null;
 
 
+    @SuppressLint({"SetTextI18n", "DefaultLocale"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,55 +36,56 @@ public class MainActivity extends AppCompatActivity {
         final SharedPreferences.Editor editor = sharedPref.edit();
 
 
-        final TextView rLabelAmount = (TextView) findViewById(R.id.amountLabel);
-        final TextView rUser = (TextView) findViewById(R.id.user_rut_label);
+        final TextView rLabelAmount = findViewById(R.id.amountLabel);
+        final TextView rUser = findViewById(R.id.user_rut_label);
 
         rLabelAmount.setText("$"+sharedPref.getString("balance", "000000"));
         String User = sharedPref.getString("user", "123456789");
         User = User.substring(0, User.length()-1)+"-"+User.substring(User.length()-1);
         rUser.setText(User);
 
-        final FloatingActionButton settingB = (FloatingActionButton) findViewById(R.id.settingButton);
-        settingB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(view.getContext(), setting.class);
-                startActivity(i);
-            }
+        final FloatingActionButton settingB = findViewById(R.id.settingButton);
+        settingB.setOnClickListener(view -> {
+            Intent i = new Intent(view.getContext(), setting.class);
+            startActivity(i);
         });
 
-        FloatingActionButton refresh = (FloatingActionButton) findViewById(R.id.refreshButton);
-        refresh.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("DefaultLocale")
-            @Override
-            public void onClick(View view) {
+        FloatingActionButton refresh = findViewById(R.id.refreshButton);
+        refresh.setOnClickListener(view -> {
 
-                Calendar cal = Calendar.getInstance();
+            Calendar cal = Calendar.getInstance();
 
-                StringBuilder URLBalance = new StringBuilder();
-                URLBalance.append("http://www.becajunaebsodexo.cl/wp-content/plugins/beneficiarios/api.php?action=balance");
-                URLBalance.append("&token="+sharedPref.getString("token", null));
-                URLBalance.append("&from=01/"+ String.format("%02d", cal.get(Calendar.MONTH)+1) +"/"+cal.get(Calendar.YEAR));
-                URLBalance.append("&to=01/"+ String.format("%02d", Calendar.MONTH + 2)  +"/"+cal.get(Calendar.YEAR));
-                URLBalance.append("&clientid=1");
-                URLBalance.append("&serviceid=15");
-                JSONParser parser = new JSONParser();
-                System.out.println(URLBalance.toString());
-                JSONObject Data = null;
-                try {
-                    Data = (JSONObject) parser.parse(getContentHTML(URLBalance.toString()));
-                    JSONObject ReturnBalance = (JSONObject) Data.get("result");
-                    JSONObject Balance = (JSONObject) ReturnBalance.get("return");
-                    editor.putString("balance", Balance.get("amountBalance").toString());
-                    editor.apply();
-                    rLabelAmount.setText("$"+sharedPref.getString("balance", "000000"));
-                    String User = sharedPref.getString("user", "123456789");
-                    User = User.substring(0, User.length()-1)+"-"+User.substring(User.length()-1);
-                    rUser.setText(User);
+            StringBuilder URLBalance = new StringBuilder();
+            URLBalance.append("http://www.becajunaebsodexo.cl/wp-content/plugins/beneficiarios/api.php?action=balance");
+            URLBalance.append("&token=");
+            URLBalance.append(sharedPref.getString("token", null));
+            URLBalance.append("&from=01/");
+            URLBalance.append(String.format("%02d", cal.get(Calendar.MONTH) + 1));
+            URLBalance.append("/");
+            URLBalance.append(cal.get(Calendar.YEAR));
+            URLBalance.append("&to=01/");
+            URLBalance.append(String.format("%02d", Calendar.MONTH + 2));
+            URLBalance.append("/");
+            URLBalance.append(cal.get(Calendar.YEAR));
+            URLBalance.append("&clientid=1");
+            URLBalance.append("&serviceid=15");
 
-                } catch (ParseException | IOException | NullPointerException e) {
-                    e.printStackTrace();
-                }
+            JSONParser parser = new JSONParser();
+            System.out.println(URLBalance.toString());
+            JSONObject Data;
+            try {
+                Data = (JSONObject) parser.parse(getContentHTML(URLBalance.toString()));
+                JSONObject ReturnBalance = (JSONObject) Data.get("result");
+                JSONObject Balance = (JSONObject) ReturnBalance.get("return");
+                editor.putString("balance", Balance.get("amountBalance").toString());
+                editor.apply();
+                rLabelAmount.setText("$" + sharedPref.getString("balance", "000000"));
+                String User1 = sharedPref.getString("user", "123456789");
+                User1 = User1.substring(0, User1.length() - 1) + "-" + User1.substring(User1.length() - 1);
+                rUser.setText(User1);
+
+            } catch (ParseException | IOException | NullPointerException e) {
+                e.printStackTrace();
             }
         });
 
--- app/src/main/java/jnet/junaebsodexo/setting.java
+++ app/src/main/java/jnet/junaebsodexo/setting.java
@@ -10,11 +10,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import org.json.JSONException;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
 
 import java.io.IOException;
 import java.util.Calendar;
@@ -24,6 +22,7 @@ public class setting extends AppCompatActivity {
 
     SharedPreferences sharedPref = null;
 
+    @SuppressLint({"SetTextI18n", "DefaultLocale"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,59 +35,63 @@ public class setting extends AppCompatActivity {
         sharedPref = getSharedPreferences("SodexoJunaeb", MODE_PRIVATE);
         final SharedPreferences.Editor editor = sharedPref.edit();
 
-        final EditText user = (EditText) findViewById(R.id.userID);
-        final EditText pass = (EditText) findViewById(R.id.passID);
-        final TextView rtokenLabel = (TextView) findViewById(R.id.token);
-        Button ConnectButton = (Button) findViewById(R.id.connectionButton);
+        final EditText user = findViewById(R.id.userID);
+        final EditText pass = findViewById(R.id.passID);
+        final TextView rtokenLabel = findViewById(R.id.token);
+        Button ConnectButton = findViewById(R.id.connectionButton);
 
-        ConnectButton.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint({"SetTextI18n", "DefaultLocale"})
-            @Override
-            public void onClick(View view) {
-                SodexoClient uClient = null;
+        ConnectButton.setOnClickListener((View view) -> {
+            SodexoClient uClient = null;
+            try {
+                uClient = new SodexoClient(user.getText().toString(), pass.getText().toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            assert uClient != null;
+            if (uClient.login) {
+                Map getToken = null;
                 try {
-                    uClient = new SodexoClient(user.getText().toString(), pass.getText().toString());
+                    getToken = uClient.getInfoToken();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                    if (uClient.login) {
-                        Map getToken = null;
-                        try {
-                            getToken = uClient.getInfoToken();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        Calendar cal = Calendar.getInstance();
-                        editor.putString("user", user.getText().toString());
-                        editor.putString("pass", pass.getText().toString());
-                        StringBuffer URLBalance = new StringBuffer();
-                        URLBalance.append("http://www.becajunaebsodexo.cl/wp-content/plugins/beneficiarios/api.php?action=balance");
-                        URLBalance.append("&token="+getToken.get("token"));
-                        URLBalance.append("&from=01/"+ String.format("%02d", cal.get(Calendar.MONTH) +1) +"/"+cal.get(Calendar.YEAR));
-                        URLBalance.append("&to=01/"+ String.format("%02d", Calendar.MONTH + 2)  +"/"+cal.get(Calendar.YEAR));
-                        URLBalance.append("&clientid=1");
-                        URLBalance.append("&serviceid=15");
-                        System.out.println(URLBalance.toString());
-                        JSONParser parser = new JSONParser();
-                        JSONObject Data = null;
-                        try {
-                        Data = (JSONObject) parser.parse(uClient.ExtractData(URLBalance.toString()));
-                        JSONObject ReturnBalance = (JSONObject) Data.get("result");
-                        JSONObject Balance = (JSONObject) ReturnBalance.get("return");
-                        editor.putString("token", (String) getToken.get("token"));
-                        editor.putString("clientid", "1");
-                        editor.putString("serviceid", "15");
-                        editor.putString("balance", Balance.get("amountBalance").toString());
-                        editor.apply();
-                        rtokenLabel.setText(getToken.get("token")+ ". SALDO: "+Balance.get("amountBalance"));
-                        } catch (ParseException | NullPointerException | IOException e) {
-                            e.printStackTrace();
-                        }
+                Calendar cal = Calendar.getInstance();
+                editor.putString("user", user.getText().toString());
+                editor.putString("pass", pass.getText().toString());
+                StringBuilder URLBalance = new StringBuilder();
+                URLBalance.append("http://www.becajunaebsodexo.cl/wp-content/plugins/beneficiarios/api.php?action=balance");
+                assert getToken != null;
+                URLBalance.append("&token=");
+                URLBalance.append(getToken.get("token"));
+                URLBalance.append("&from=01/");
+                URLBalance.append(String.format("%02d", cal.get(Calendar.MONTH) + 1));
+                URLBalance.append("/");
+                URLBalance.append(cal.get(Calendar.YEAR));
+                URLBalance.append("&to=01/");
+                URLBalance.append(String.format("%02d", Calendar.MONTH + 2));
+                URLBalance.append("/");
+                URLBalance.append(cal.get(Calendar.YEAR));
+                URLBalance.append("&clientid=1");
+                URLBalance.append("&serviceid=15");
+                System.out.println(URLBalance.toString());
+                JSONParser parser = new JSONParser();
+                JSONObject Data;
+                try {
+                    Data = (JSONObject) parser.parse(uClient.ExtractData(URLBalance.toString()));
+                    JSONObject ReturnBalance = (JSONObject) Data.get("result");
+                    JSONObject Balance = (JSONObject) ReturnBalance.get("return");
+                    editor.putString("token", (String) getToken.get("token"));
+                    editor.putString("clientid", "1");
+                    editor.putString("serviceid", "15");
+                    editor.putString("balance", Balance.get("amountBalance").toString());
+                    editor.apply();
+                    rtokenLabel.setText(getToken.get("token") + ". SALDO: " + Balance.get("amountBalance"));
+                } catch (ParseException | NullPointerException | IOException e) {
+                    e.printStackTrace();
+                }
 
-                    }
             }
         });
-
 
 
     }
